6f90632a9101f450edac9e164755913b11d9d1c6
vinod <113596145+pathivinod@users.noreply.github.com>
2023-12-01 22:55:33 +0530

    Create operator overidding.java

---
+++ operator overidding.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+final class ram
+{
+void display()
+{
+System.out.println("name");
+}
+}
+class ragh extends ram
+{
+void display2()
+{
+System.out.println("raf");
+}
+}
+class me
+{
+public static void main(String args[])
+{
+ragh a=new ragh();
+a.display2();
+}
+}
