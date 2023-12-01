78ee131a726f533a37599f820d177715a476a1e1
vinod <113596145+pathivinod@users.noreply.github.com>
2023-12-01 21:47:55 +0530

    Create switchcase.java

---
+++ switchcase.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+class switchcase
+{
+public static void main(String args[])
+{
+int c;
+Scanner s=new Scanner(System.in);
+System.out.println("enter your choice=");
+c=s.nextInt();
+switch(c)
+{
+case 1:
+System.out.println("RAM CHARAN");
+break;
+case 2:
+System.out.println("young tiger NTR");
+break;
+case 3:
+System.out.println("SIVA royal");
+break;
+case 4:
+System.out.println("Sai");
+break;
+default:
+System.out.println("vinod");
+}
+}
+}
