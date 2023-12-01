6fa4d8dbd09a99007f56c110fe8c7bdb3ce098c8
vinod <113596145+pathivinod@users.noreply.github.com>
2023-12-01 22:49:42 +0530

    Create squares and cubes of number.java

---
+++ squares and cubes of number.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class sqc
+{
+public static void main(String args[])
+{
+float n;
+double sq,c;
+Scanner s=new Scanner(System.in);
+System.out.println("enter n value=");
+n=s.nextFloat();
+sq=n*n;
+c=n*n*n;
+System.out.println("square number="+sq);
+System.out.println("cube number ="+c);
+}}
