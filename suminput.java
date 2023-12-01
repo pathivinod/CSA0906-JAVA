dadfd5711ed2e6d3fc2f2d51fd86f96102efc824
vinod <113596145+pathivinod@users.noreply.github.com>
2023-12-01 06:40:57 +0530

    Create suminput.java

---
+++ suminput.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class suminput
+{
+public static void main(String args[])
+{
+int x,y;
+Scanner s=new Scanner(System.in);
+System.out.println("enter x and y values=");
+x=s.nextInt();
+y=s.nextInt();
+int z=x+y;
+System.out.println("sum of 2 numbers ="+z);
+}
+}
